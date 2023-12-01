306c9082e25a370a6dd37b020d44f9bf1e835f42
horseman <koen.de.ruiter@hotmail.com>
2023-12-01 12:43:38 +0100

    Did Puzzle 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import puzzles.puzzle1
+import java.io.File
+
+fun main(args: Array<String>) {
+    println(puzzle1())
+}
---
+++ src/main/kotlin/puzzles/puzzle1.kt
@@ -0,0 +1,29 @@
+package puzzles
+
+import java.io.File
+
+fun puzzle1() : Int {
+    var sum = 0
+    File("src/main/resources/inputs/puzzle1.txt").forEachLine {
+        val numbers = mutableListOf(-1, -1)
+        for(char in it) {
+            if(char.digitToIntOrNull() != null) {
+                if(numbers[0] == -1) {
+                    numbers[0] = char.digitToInt()
+                }
+                else {
+                    numbers[1] = char.digitToInt()
+                }
+            }
+        }
+        sum += if(numbers[1] == -1) {
+            11 * numbers[0]
+        } else {
+            10 * numbers[0] + numbers[1]
+        }
+//        println(sum)
+//        println(it)
+//        readln()
+    }
+    return sum
+}
