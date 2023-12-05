e6a64a94898db5fe9cbfa565f82992f09fb309d8
horseman <koen.de.ruiter@hotmail.com>
2023-12-05 17:56:37 +0100

    Day 5 code doesn't work anymore, but I got the right answer???

--- src/main/kotlin/puzzles/Day5.kt
+++ src/main/kotlin/puzzles/Day5.kt
@@ -1,69 +1,28 @@
 package puzzles
 
-import print
 import println
 import readInput
+import java.lang.IndexOutOfBoundsException
 
 fun main() {
 //    part1(readInput("day5")).println()
-//    part2(readInput("day5")).println()
-
-    part1(listOf(
-        "seeds: 79 14 55 13",
-        "",
-        "seed-to-soil map:",
-        "50 98 2",
-        "52 50 48"
-    )).println()
-
-//    part1(listOf(
-//        "seeds: 79 14 55 13",
-//        "",
-//        "seed-to-soil map:",
-//        "50 98 2",
-//        "52 50 48",
-//        "",
-//        "soil-to-fertilizer map:",
-//        "0 15 37",
-//        "37 52 2",
-//        "39 0 15",
-//        "",
-//        "fertilizer-to-water map:",
-//        "49 53 8",
-//        "0 11 42",
-//        "42 0 7",
-//        "57 7 4",
-//        "",
-//        "water-to-light map:",
-//        "88 18 7",
-//        "18 25 70",
-//        "",
-//        "light-to-temperature map:",
-//        "45 77 23",
-//        "81 45 19",
-//        "68 64 13",
-//        "",
-//        "temperature-to-humidity map:",
-//        "0 69 1",
-//        "1 0 69",
-//        "",
-//        "humidity-to-location map:",
-//        "60 56 37",
-//        "56 93 4",
-//        )).println()
+    part2(readInput("day5")).println()
 }
 
 
 private fun part1(input: List<String>): Long {
     val seeds : MutableList<MutableList<Long>> = mutableListOf()
     seeds.add(mutableListOf())
-    val maps : MutableList<MutableMap<Pair<Long, Long>, Pair<Long, Long>>> = mutableListOf()
+    seeds.add(mutableListOf())
+    val maps : MutableList<MutableMap<Pair<Long, Long>, Long>> = mutableListOf()
     var searchNumbers = false
+
     for(line in input) {
         if(line.split(": ").size == 2) {
             val stringList = line.split(": ")[1].split(" ")
             for(item in stringList) {
-                seeds.last().add(item.toLong())
+                seeds[0].add(item.toLong())
+                seeds[1].add(-1)
             }
         }
 
@@ -78,10 +37,7 @@ private fun part1(input: List<String>): Long {
                 mappingData.add(item.toLong())
             }
 
-            maps.last()[Pair(mappingData[1], mappingData[1] + mappingData[2])] = Pair(mappingData[0], mappingData[0] + mappingData[2])
-            Pair(mappingData[1], mappingData[1] + mappingData[2]).println()
-            Pair(mappingData[0], mappingData[0] + mappingData[2]).println()
-            println()
+            maps.last()[Pair(mappingData[1], mappingData[1] + mappingData[2])] = mappingData[0] - mappingData[1]
         }
 
         else if(line.contains("map")) {
@@ -90,36 +46,122 @@ private fun part1(input: List<String>): Long {
         }
     }
 
+    "Data gathered, starting map conversion now.".println()
+
+    var seedList = 1
     for(map in maps) {
-        seeds.add(mutableListOf())
-        for(seed in seeds[seeds.size - 2]) {
+        for(index in seeds[seedList].indices) {
             var seedChecked = false
             for(entry in map) {
-                val index = getIndexOfSeed(seed, entry.key)
-                entry.println()
-                if(index != -1) {
-                    seeds.last().add(entry.value.first + seed)
+                if(entry.key.first <= seeds[1 - seedList][index] && seeds[1 - seedList][index] < entry.key.second) {
+                    seeds[seedList][index] = seeds[1 - seedList][index] + entry.value
                     seedChecked = true
                 }
             }
             if(!seedChecked) {
-                seeds.last().add(seed)
+                seeds[seedList][index] = seeds[1 - seedList][index]
             }
+        }
+        if(seedList == 0) {
+            seedList = 1
+        }
+        else if(seedList == 1) {
+            seedList = 0
         }
     }
 
 
-    return seeds.last().min()
+    return seeds[1 - seedList].min()
 }
 
-fun getIndexOfSeed(seed:Long, entry:Pair<Long, Long>) : Int {
-    if(entry.first <= seed && seed < entry.second) {
-        seed.println()
-        entry.println()
-        (seed - entry.first).toInt().println()
-        println()
-        return (seed - entry.first).toInt()
-    } else {
-        return -1
+private fun part2(input: List<String>) : Long {
+    val minimalLocations : MutableList<Long> = mutableListOf()
+    val rangesString = input[0].split(": ")[1].split(" ")
+    val ranges = mutableListOf<Long>()
+    for(item in rangesString) {
+        ranges.add(item.toLong())
     }
+
+
+    for(i in input[0].split(": ")[1].split(" ").indices step 2) {
+        while(ranges[1 + 1] > 10_000_000) {
+
+            if (ranges[1 + 1] <= 10_000_000) {
+                minimalLocations.add(getMinOfPart(input, ranges[i], ranges[i + 1]))
+            } else {
+                minimalLocations.add(getMinOfPart(input, ranges[i], ranges[i + 1] + 10_000_000))
+            }
+
+            ranges[i] += 10_000_000L
+            ranges[i + 1] -= 10_000_000L
+
+        }
+    }
+
+    return minimalLocations.min()
+}
+
+private fun getMinOfPart(input: List<String>, min: Long, max: Long) : Long{
+    val seeds : MutableList<MutableList<Long>> = mutableListOf()
+    seeds.add(mutableListOf())
+    seeds.add(mutableListOf())
+    val maps : MutableList<MutableMap<Pair<Long, Long>, Long>> = mutableListOf()
+    var searchNumbers = false
+
+    for(line in input) {
+        if(line.split(": ").size == 2) {
+            for(i in min .. min + max) {
+                seeds[0].add(i)
+                seeds[1].add(-1)
+            }
+        }
+
+        else if(line.isEmpty()) {
+            searchNumbers = false
+        }
+
+        else if(searchNumbers) {
+            val mappingData : MutableList<Long> = mutableListOf()
+
+            for(item in line.split(" ")) {
+                mappingData.add(item.toLong())
+            }
+
+            maps.last()[Pair(mappingData[1], mappingData[1] + mappingData[2])] = mappingData[0] - mappingData[1]
+        }
+
+        else if(line.contains("map")) {
+            maps.add(mutableMapOf())
+            searchNumbers = true
+        }
+    }
+
+
+    "Data gathered, starting map conversion now.".println()
+
+
+    var seedList = 1
+    for(map in maps) {
+        for(index in seeds[seedList].indices) {
+            var seedChecked = false
+            for(entry in map) {
+                if(entry.key.first <= seeds[1 - seedList][index] && seeds[1 - seedList][index] < entry.key.second) {
+                    seeds[seedList][index] = seeds[1 - seedList][index] + entry.value
+                    seedChecked = true
+                }
+            }
+            if(!seedChecked) {
+                seeds[seedList][index] = seeds[1 - seedList][index]
+            }
+        }
+        if(seedList == 0) {
+            seedList = 1
+        }
+        else if(seedList == 1) {
+            seedList = 0
+        }
+    }
+
+
+    return seeds[1 - seedList].min()
 }
